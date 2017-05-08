8948373f817336db0e77a94ebf0d0d45c19d84d0
Luv <Luv.singh@sandisk.com>
2017-05-08 15:55:14 +0530

    Removes adaptive thresholding from preprocessing.

--- Src/screen/CaptureUtil.py
+++ Src/screen/CaptureUtil.py
@@ -23,11 +23,12 @@ class CaptureUtil:
         Return all strings in an image as a string.
 
         """
-        basewidth = 6400
+        basewidth = 3096
         img = Image.open(img_path)
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        img = img.resize((img.size[0], img.size[1]), Image.ANTIALIAS)
         img.save(os.path.join(self.path, 'zoomed.jpg'))
         img = cv2.imread(os.path.join(self.path, 'zoomed.jpg'))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -35,10 +36,10 @@ class CaptureUtil:
         img = cv2.dilate(img, kernel, iterations=1)
         img = cv2.erode(img, kernel, iterations=1)
         cv2.imwrite(os.path.join(self.path, "removed_noise.png"), img)
-        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                    cv2.THRESH_BINARY, 11, 2)
+        # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        #                             cv2.THRESH_BINARY, 11, 2)
         cv2.imwrite(os.path.join(self.path, "thres.png"), img)
-        result = pytesseract.image_to_string(Image.open(os.path.join(self.path, "thres.png")))
+        result = pytesseract.image_to_string(Image.open(os.path.join(self.path, "thres.png")), lang="eng")
         return result
 
 
